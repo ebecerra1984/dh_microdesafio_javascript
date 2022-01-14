1eb09c9961c18b51147ed6013054974e68d4a3a5
ebecerra1984 <58277040+ebecerra1984@users.noreply.github.com>
2022-01-14 10:11:05 -0300

    First commit

---
+++ public/js/moviesAdd.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+  let titulo = document.querySelector(".moviesAddTitulo");
+  let formulario = document.querySelector("#formulario");
+  let article = document.querySelector("article");
+  titulo.innerHTML = "AGREGAR PELÍCULA";
+  titulo.classList.add("titulo");
+  article.classList.add("fondoTransparente");
+  formulario.classList.add("fondoCRUD");
+};
+
+let estadoSecreto = 0;
+const tituloInput = document.querySelector("#titulo");
+tituloInput.addEventListener("keyup", () => {
+  switch (tituloInput.value) {
+    case "s":
+      estadoSecreto = 1;
+      break;
+    case "se":
+      estadoSecreto = 2;
+      break;
+    case "sec":
+      estadoSecreto = 3;
+      break;
+    case "secr":
+      estadoSecreto = 4;
+      break;
+    case "secre":
+      estadoSecreto = 5;
+      break;
+    case "secret":
+      estadoSecreto = 6;
+      break;
+    case "secreto":
+      alert("SECRETO MAGICO");
+      estadoSecreto = 0;
+      break;
+    default:
+      tituloInput.value = "";
+      estadoSecreto = 0;
+  }
+  console.log(tituloInput.value);
+  console.log(estadoSecreto);
+});
