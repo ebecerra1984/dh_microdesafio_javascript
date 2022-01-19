0a512a7772637ec1792088f3f632636e0ad41185
ebecerra1984 <58277040+ebecerra1984@users.noreply.github.com>
2022-01-18 23:25:37 -0300

    calse is-valid agregada

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -14,31 +14,37 @@ window.onload = function () {
       errores.push("Awards debe estar completo");
       formulario.awards.classList.add("is-invalid");
     } else {
+      formulario.awards.classList.add("is-valid");
     }
     if (datosForm.title === "") {
       errores.push("Titulo debe estar completo");
       formulario.title.classList.add("is-invalid");
     } else {
+      formulario.title.classList.add("is-valid");
     }
     if (datosForm.genre_id === "") {
       errores.push("Genero debe estar completo");
       formulario.genre_id.classList.add("is-invalid");
     } else {
+      formulario.genre_id.classList.add("is-valid");
     }
     if (datosForm.rating === "") {
       errores.push("Calificacion debe estar completo");
       formulario.rating.classList.add("is-invalid");
     } else {
+      formulario.rating.classList.add("is-valid");
     }
     if (datosForm.release_date === "") {
       errores.push("Fecha de estreno debe estar completo");
       formulario.release_date.classList.add("is-invalid");
     } else {
+      formulario.release_date.classList.add("is-valid");
     }
     if (datosForm.length === "") {
       errores.push("Duracion debe estar completo");
       formulario.length.classList.add("is-invalid");
     } else {
+      formulario.length.classList.add("is-valid");
     }
 
     if (errores.length > 0) {
