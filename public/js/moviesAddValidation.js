8493543bd0dea7d62b6a9de52b4f44da35bd38aa
ebecerra1984 <58277040+ebecerra1984@users.noreply.github.com>
2022-01-18 23:14:36 -0300

    Agragado de clase is-invalid

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -3,6 +3,7 @@ window.onload = function () {
   titulo.focus();
 
   const formulario = document.querySelector("form");
+  console.log(formulario.title);
 
   formulario.addEventListener("submit", (e) => {
     // e.preventDefault();
@@ -11,21 +12,32 @@ window.onload = function () {
 
     if (datosForm.awards === "") {
       errores.push("Awards debe estar completo");
+      formulario.awards.classList.add("is-invalid");
     } else {
     }
     if (datosForm.title === "") {
+      errores.push("Titulo debe estar completo");
+      formulario.title.classList.add("is-invalid");
     } else {
     }
     if (datosForm.genre_id === "") {
+      errores.push("Genero debe estar completo");
+      formulario.genre_id.classList.add("is-invalid");
     } else {
     }
     if (datosForm.rating === "") {
+      errores.push("Awards debe estar completo");
+      formulario.awards.classList.add("is-invalid");
     } else {
     }
     if (datosForm.release_date === "") {
+      errores.push("Awards debe estar completo");
+      formulario.awards.classList.add("is-invalid");
     } else {
     }
     if (datosForm.length === "") {
+      errores.push("Awards debe estar completo");
+      formulario.awards.classList.add("is-invalid");
     } else {
     }
 
